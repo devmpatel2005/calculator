db7c2579608684718713ad2cf96dc4bfd3e60aeb
devmpatel2005 <devmpatel2005@gmail.com>
2025-03-04 20:05:51 -0500

    Basic Operations added

---
+++ calc.js
@@ -0,0 +1,34 @@
+function add (a, b){
+    return + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a/b;
+}
+
+let num1;
+let num2;
+let operation;
+
+function operate(a, b, operation){
+    if(String(operation) == '+'){
+        return add(a, b);
+    }
+    else if(String(operation) == '-'){
+        return subtract(a, b);
+    }
+    else if(String(operation) == '*'){
+        return multiply(a, b);
+    }
+    else if(String(operation) == '/'){
+        return divide(a, b);
+    }
+}
